0c3c54cd5801cd89decc1260597f92b298ff7479
Rishal Hurbans <rishal.hurbans@gmail.com>
2017-02-20 17:59:58 +0200

    Added file input/output.

--- src/main/java/com/jedi/hackathons/App.java
+++ src/main/java/com/jedi/hackathons/App.java
@@ -1,13 +1,43 @@
 package com.jedi.hackathons;
 
-/**
- * Hello world!
- *
- */
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args){
+
+        //Open file connections
+        Scanner in = null;
+        BufferedWriter out = null;
+        try{
+            in = new Scanner(new File("A-small-attempt4.in"));
+            out = new BufferedWriter(new FileWriter("result.txt" , false));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+        //Variables
+        int n;
+        String output = "";
+        //number of cases
+        n = in.nextInt();
+        for(int i = 0; i < n; i++){
+            output += "Case #" + (i+1) + ": ";
+            if(i != n-1) output += '\n';
+        }
+
+
+        //output and close
+        try{
+            out.write(output);
+            in.close();
+            out.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
