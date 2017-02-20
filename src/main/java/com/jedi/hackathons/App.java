9c204987375d1437eca099a8a9a9524334707100
Rishal Hurbans <rishal.hurbans@gmail.com>
2017-02-20 18:02:46 +0200

    Cleaned up some magic strings.

--- src/main/java/com/jedi/hackathons/App.java
+++ src/main/java/com/jedi/hackathons/App.java
@@ -7,14 +7,17 @@ import java.util.Scanner;
 
 public class App 
 {
+    private static final String INPUT_FILE_NAME = "A-small-attempt4.in";
+    private static final String OUTPUT_FILE_NAME = "result.txt";
+
     public static void main(String[] args){
 
         //Open file connections
         Scanner in = null;
         BufferedWriter out = null;
         try{
-            in = new Scanner(new File("A-small-attempt4.in"));
-            out = new BufferedWriter(new FileWriter("result.txt" , false));
+            in = new Scanner(new File(INPUT_FILE_NAME));
+            out = new BufferedWriter(new FileWriter(OUTPUT_FILE_NAME , false));
         }catch(Exception e){
             e.printStackTrace();
         }
@@ -31,7 +34,7 @@ public class App
         }
 
 
-        //output and close
+        //Output and close
         try{
             out.write(output);
             in.close();
